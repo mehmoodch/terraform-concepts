62ef789d9e6bc26af894c95a67c70e96ba5eb33a
Steve Jason Developer <mehmoodasgharch@gmail.com>
2024-07-20 16:15:01 +0400

    initial commit

--- output.tf
+++ output.tf
@@ -1,24 +1,3 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.terraform-test-vpc.id
-}
-
-output "public_subnet_ids" {
-  description = "The IDs of the public subnets"
-  value       = aws_subnet.public-subnet[*].id
-}
-
-output "private_subnet_ids" {
-  description = "The IDs of the private subnets"
-  value       = aws_subnet.private-subnet[*].id
-}
-
-output "public_instance_id" {
-  description = "The ID of the public instance"
-  value       = aws_instance.public_instance.id
-}
-
-output "private_instance_id" {
-  description = "The ID of the private instance"
-  value       = aws_instance.private_instance.id
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:27d153dd770c779132417dd95babeedfccfae3459b1f93bad7e8f5f348234471
+size 644
